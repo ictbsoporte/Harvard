ebe0be3a3015a24f826e2737e4468014f6b33a8a
ictbsoporte <107148238+ictbsoporte@users.noreply.github.com>
2022-06-09 13:13:27 -0500

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,40 +1,24 @@
 <?php
 
-    
-    $from = $_POST['email'];
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $number = $_POST['number'];
-    $cmessage = $_POST['message'];
+            		$to = 'ictbsoporte@gmail.com'; // replace this mail with yours
+          		$firstname = $_POST["fname"];
+          		$email= $_POST["email"];
+          		$headers = 'MIME-Version: 1.0' . "\r\n";
+          		$headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+          		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+          		$message ='
+'.$firstname.'  '.$laststname.'
+Email: '.$email.'
+';
 
-    $subject = "You have a message from your Bitmap Photography.";
-	$mensaje = "ests es un mensaje enviado por". $name . ",\r\n";
-	$mensaje = "su correo es " . $from . "\r\n";
-	$mensaje = "mensaje" . $_POST['message'] . "\r\n";
-	$to = "ictbsoporte@gmail.com";
-	$asunto = "Correo pagina web";
-	$logo = 'img/logo.png';
-    $link = '#';
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+          		if (@mail($to, $email, $message, $headers))
+          		{
+          			echo 'The message has been sent.';
+          		}else{
+          		echo 'failed';
+          	}
+          
 	
     $send = mail($to, $subject, $body, $headers);
 	header("Location:index.html");
